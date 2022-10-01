7d49d1433d95af267552d5110cd1572e176ee658
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-10-01 10:55:34 -0700

    Part 2 Activity

--- public/index.js
+++ public/index.js
@@ -7,15 +7,34 @@ async function main(){
         let db = await res.json()
         console.log(db)
 
+    async function updateValue (val) {
+        let res = await fetch('http://localhost:9001/counter',
+        {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: val
+            })
+        })
+        
+        let newValue = await res.json()
+        return newValue.value
+    }
+    
+
     let countValue = db.value;
 
-    function increment(){
-        countValue++;
+    async function increment(){
+        let newValue = await updateValue(countValue + 1)
+        countValue = newValue;
         countContainer.textContent = countValue;
     }
 
-    function decrement(){
-        countValue--;
+    async function decrement(){
+        let newValue = await updateValue(countValue - 1)
+        countValue = newValue;
         countContainer.textContent = countValue;
     }
 
